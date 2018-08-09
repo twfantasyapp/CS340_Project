97a4d0ce3fc8e592f7ae7119d9d8cf97d98057e5
twfantasyapp <42125910+twfantasyapp@users.noreply.github.com>
2018-08-09 02:25:05 -0700

    Add files via upload

--- players.js
+++ players.js
@@ -10,6 +10,19 @@ module.exports = function(){
     var router = express.Router();
 
 
+    /*function to return teams in the drop-down list*/
+    function getTeams(res, mysql, context, complete){
+        mysql.pool.query("SELECT id, name FROM prj_Team", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.teams  = results;
+            complete();
+        });
+    }
+
+
     /*function to return the table of the all players (default)*/
     function getPlayers(res, mysql, context, complete){
         mysql.pool.query("SELECT p.id as pId, lastName, firstName, t.name as tName, 2ptFGs, 3ptFGs, rebounds, assists, steals, (2ptFGs + 3ptFGs + rebounds + assists + steals) as `total` FROM prj_Player p INNER JOIN prj_Team t ON p.team = t.id ORDER BY `total` DESC", function(error, results, fields){
@@ -60,6 +73,20 @@ module.exports = function(){
         });
     }
 
+    /*function to return a single player*/
+    function getOnePlayer(res, mysql, context, id, complete){
+        var sql = "SELECT id, firstName, lastName, team, 2ptFGs, 3ptFGs, rebounds, assists, steals FROM prj_Player WHERE id = ?";
+        var inserts = [id];
+        mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.player = results[0];
+            complete();
+        });
+    }
+
     /*Display all players (default). Requires web based javascript to delete players with AJAX*/
     router.get('/', function(req, res){
         var callbackCount = 0; 
@@ -67,9 +94,10 @@ module.exports = function(){
        // context.jsscripts = ["deleteplayers.js"];
         var mysql = req.app.get('mysql');
         getPlayers(res, mysql, context, complete);
+        getTeams(res, mysql, context, complete);
         function complete(){
             callbackCount++;
-            if(callbackCount >= 1){
+            if(callbackCount >= 2){
                 res.render('players', context);
             }
 
@@ -82,9 +110,10 @@ module.exports = function(){
         var context = {};
         var mysql = req.app.get('mysql');
         sortPlayers(req, res, mysql, context, complete);
+        getTeams(res, mysql, context, complete);
         function complete(){
             callbackCount++;
-            if(callbackCount >= 1){
+            if(callbackCount >= 2){
                 res.render('players', context);
             }
         }
@@ -96,15 +125,34 @@ module.exports = function(){
         var context = {};
         var mysql = req.app.get('mysql');
         filterPlayers(req, res, mysql, context, complete);
+        getTeams(res, mysql, context, complete);
         function complete(){
             callbackCount++;
-            if(callbackCount >= 1){
+            if(callbackCount >= 2){
                 res.render('players', context);
             }
         }
     });
 
-    /* Route to delete a player, simply returns a 202 upon success. Ajax will handle this. */
+    /* Adds a player, redirects to the players page after adding */
+    router.post('/addPlayer', function(req, res){
+        //console.log(req.body.homeworld)
+        //console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO prj_Player (firstName, lastName, team) VALUES (?,?,?)";
+        var inserts = [req.body.firstName, req.body.lastName, req.body.team];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/players');
+            }
+        });
+    });
+
+    /*Route to delete a player*/
     router.get('/delete', function(req, res){
         var callbackCount = 0;
         var context = {};
@@ -118,6 +166,41 @@ module.exports = function(){
         }
     });
 
+
+    /* Display one player for the specific purpose of updating players */
+    router.get('/:id', function(req, res){
+        callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["selectedteam.js", "updateplayer.js"];
+        var mysql = req.app.get('mysql');
+        getOnePlayer(res, mysql, context, req.params.id, complete);
+        getTeams(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('update-players', context);
+            }
+
+        }
+    });
+
+
+   /* The URI that update data is sent to in order to update a person */
+    router.put('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "UPDATE prj_Player SET firstName=?, lastName=?, team=?, 2ptFGs=?, 3ptFGs=?, rebounds=?, assists=?, steals=? WHERE id=?";
+        var inserts = [req.body.firstName, req.body.lastName, req.body.team, req.body.twoptFG, req.body.thrptFG, req.body.rebounds, req.body.assists, req.body.steals, req.params.id];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.status(200);
+                res.end();
+            }
+        });
+    });
 
     return router;
 }();
